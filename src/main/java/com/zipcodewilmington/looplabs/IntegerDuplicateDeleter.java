a751a4a5b0f9d5de22130bb8892fab7fdc61ba8a
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-27 17:44:58 -0400

    Integer exact Duplicate complete

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,44 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String nonDuplicatesString = "";
+
+        for (int i = 0; i < array.length; i++) {
+            Integer integer = array[i];
+            Integer counter = 0;
+
+            for(int j = 0; j < array.length; j++){
+                if(integer.equals(array[j])){
+                    counter++;
+                }
+            }
+            if(!(counter == exactNumberOfDuplications)){
+                nonDuplicatesString += array[i] + " ";
+            }
+        }
+        String[] stringResult = nonDuplicatesString.split(" ");
+        Integer[] result = new Integer[stringResult.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.valueOf(stringResult[i]);
+
+        }
+
+
+
+        return result;
+    }
 }
